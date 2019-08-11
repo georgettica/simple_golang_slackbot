67c8016425a02d02b816234aefdd9a180f833964
Ron Green <11993626+georgettica@users.noreply.github.com>
2019-08-12 00:16:44 +0300

    make it work!

--- main.go
+++ main.go
@@ -22,4 +22,38 @@ func main() {
   api := slack.New(token)
   rtm := api.NewRTM()
   go rtm.ManageConnection()
+
+Loop:
+  for {
+    select {
+    case msg := <-rtm.IncomingEvents:
+      fmt.Print("Event Received: ")
+      switch ev := msg.Data.(type) {
+
+      case *slack.MessageEvent:
+        info := rtm.GetInfo()
+
+        text := ev.Text
+        text = strings.TrimSpace(text)
+        text = strings.ToLower(text)
+
+        matched, _ := regexp.MatchString("dark souls", text)
+
+        if ev.User != info.User.ID && matched {
+          rtm.SendMessage(rtm.NewOutgoingMessage("\\[T]/ Praise the Sun \\[T]/", ev.Channel))
+        }
+
+      case *slack.RTMError:
+        fmt.Printf("Error: %s\n", ev.Error())
+
+      case *slack.InvalidAuthEvent:
+        fmt.Printf("Invalid credentials")
+        break Loop
+
+      default:
+        // Take no action
+      }
+    }
+  }
 }
+
